5d811f6eb46d04177387e9af4342cba6b3d44435
himanshu25800 <pal.himanshu@thinksys.com>
2025-03-06 18:38:43 +0530

    implement rerunnable query

--- week3Project/script.py
+++ week3Project/script.py
@@ -18,7 +18,14 @@ class DBManage:
         except Exception as e:
             print(f"Database not connected\n {e}")
 
-    
+    def createTable(self, query):
+        try:
+            self.cursor.execute(query)
+            self.cursor.connection.commit()
+            print("Table created successfully")
+        except Exception as e:      
+            print(f"Error creating table: {e}")
+
     def get_tables(self):
         try:
             self.cursor.execute("""
@@ -96,16 +103,42 @@ class DBManage:
 
 if __name__=="__main__":
     obj = DBManage("Employee","postgres","thinksys@123","localhost")
-    obj.get_tables()
+    # obj.get_tables()
 
-    res = obj.executeSelect("Select * from departments")
-    print(res)
+    res = createTableQuery = """
+        CREATE TABLE IF NOT EXISTS departments(
+            departmentId SERIAL PRIMARY KEY,
+            departmentName VARCHAR(50) NOT NULL
+        );
+    """
 
+    # res = obj.executeSelect("Select * from employeedepartment")
+    # print(res)
+
+    # res = obj.executeSelect("Select * from employee")
+    # print(res)
+
+    # res = obj.executeSelect("Select * from departments")
+    # print(res)
+
+    # res = obj.executeSelect("Select * from salaries")
+    # print(res)
+
+
+    # employee name of employees working in Development department
+    # res = obj.executeSelect("select firstname , lastname from employee where employeeId in (select employeeId from employeedepartment where departmentId in (select departmentId from departments where departmentName = %s))",('Development',))
+    # print(res)
+    
     # res = obj.executeSelect("Select * from departments where departmentId = %s",(2,))
     # print(res)
 
 
-    # res = obj.executeInsert("Insert into departments values (%s , %s)",(4,'Qualiny Analyst'))
+
+    # res = obj.executeInsert("Insert into departments select %s , %s where not exists (select 1 from departments where departmentId =%s)",(4,'Quality Analyst',4))
+    # print(res)
 
     # res = obj.executeUpdate("Update departments set departmentName = %s where departmentName=%s",('Quality Analyst' , 'Qualiny Analyst'))
+    # print(res)
+
+    # res = obj.executeDelete("delete from departments where departmentId = %s",(4,))
     # print(res)
